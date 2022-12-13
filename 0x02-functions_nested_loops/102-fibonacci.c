a3ffcb1ad42a0902be4aa728204b991e4412c562
Brightini <brightiniabasi@gmail.com>
2022-12-14 00:48:08 +0100

    Update code :ok_hand:

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,26 +8,26 @@
  */
 void first_50(int n)
 {
-    int i, j, k, count;
+	int i, j, k, count;
 
-    count = 2;
-    printf("%d, %d, ", i = 0, j = 1);
-    while (count < n)
-    {
-        k = i + j;
-        if (k < 0)
-            k *= -1;
-        printf("%d", k);
-        if (count == n - 1)
-        {
-            printf("\n");
-            break;
-        }
-        printf(", ");
-        i = j;
-        j = k;
-        count++;
-    }
+	count = 2;
+	printf("%d, %d, ", i = 1, j = 2);
+	while (count < n)
+	{
+		k = i + j;
+		if (k < 0)
+			k = -(k);
+		printf("%d", k);
+		if (count == n - 1)
+		{
+			printf("\n");
+			break;
+		}
+		printf(", ");
+		i = j;
+		j = k;
+		count++;
+	}
 }
 
 /**
@@ -37,6 +37,6 @@ void first_50(int n)
  */
 int main(void)
 {
-    first_50(50);
-    return (0);
+	first_50(50);
+	return (0);
 }
