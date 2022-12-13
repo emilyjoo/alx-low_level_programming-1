a56fd701f2a8e0b64e3902ea3fbfbd0b5a66dc54
Brightini <brightiniabasi@gmail.com>
2022-12-13 20:41:33 +0100

    Add header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _abs(int);
+int add(int, int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+void jack_bauer(void);
+int print_sign(int n);
+void print_to_98(int n);
+int print_last_digit(int);
+void print_alphabet(void);
+void print_times_table(int n);
+void print_alphabet_x10(void);
+
+
+#endif
