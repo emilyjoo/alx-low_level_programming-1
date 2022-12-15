d44eb0d2fd0017e63ccfbd1fd01d9d5925711541
Brightini <brightiniabasi@gmail.com>
2022-12-15 16:22:53 +0100

    Update if condition :ok_hand:

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,14 +12,14 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 100 == 0))
+	if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0))
 	{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && (day - 31 == 29) && (year % 4 != 0))
+		if ((month == 2) && (day - 31 == 29))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
