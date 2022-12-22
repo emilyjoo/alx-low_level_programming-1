15f32fe2c6a27d35713ba5528eaea26d75567f80
Brightini <brightiniabasi@gmail.com>
2022-12-22 14:17:37 +0100

    Refactor code :hammer:

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -21,32 +21,16 @@ int _strcmp(char *s1, char *s2)
 		;
 
 	if (size_s1 < size_s2)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			if (s1[i] < s2[i])
-				return (-15);
-			else if (s2[i] < s1[i])
-				return (15);
-		}
-	}
-	if (size_s1 > size_s2)
+		return (-15);
+	else if (size_s1 > size_s2)
+		return (15);
+	else if (size_s1 == size_s2)
 	{
 		for (i = 0; s2[i] != '\0'; i++)
 		{
 			if (s1[i] < s2[i])
 				return (-15);
-			else if (s2[i] < s1[i])
-				return (15);
-		}
-	}
-	if (size_s1 == size_s2)
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-		{
-			if (s1[i] < s2[i])
-				return (-15);
-			else if (s2[i] < s1[i])
+			else if (s1[i] > s2[i])
 				return (15);
 		}
 	}
