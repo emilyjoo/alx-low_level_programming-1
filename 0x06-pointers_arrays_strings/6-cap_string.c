fc264adab00765b4a3c17679cc4dc898f3718fba
Brightini <brightiniabasi@gmail.com>
2022-12-23 13:44:19 +0100

    Add condition for first letter in string :ok_hand:

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,9 @@ char *cap_string(char *c)
 	int i, j;
 	char *seperator = " \t\n,;.!?\"(){}";
 
+	if (c[0] >= 97 && c[0] <= 122)
+		c[0] -= 32;
+
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		for (j = 0; seperator[j] != '\0'; j++)
