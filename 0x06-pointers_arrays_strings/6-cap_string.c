3dc628d38ab9df09a2cecbba9af0e5e329a07e41
Brightini <brightiniabasi@gmail.com>
2022-12-23 13:10:31 +0100

    Change tab to space

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,10 +9,12 @@
 char *cap_string(char *c)
 {
 	int i, j;
-	char *seperator = " \t\n,;.!?\"(){}";
+	char *seperator = "\t \n,;.!?\"(){}";
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
+		if (c[i] == '\t')
+			c[i] = ' ';
 		for (j = 0; seperator[j] != '\0'; j++)
 		{
 			if (c[i] == seperator[j] && (c[i + 1] >= 97 && c[i + 1] <= 122))
