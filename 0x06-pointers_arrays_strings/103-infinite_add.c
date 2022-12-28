9f729f9772155782747570a06642c6fe70314321
Brightini <brightiniabasi@gmail.com>
2022-12-28 14:38:29 +0100

    Refactor code :construction:

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,114 @@
 #include "main.h"
+#include <stdlib.h>
 
-unsigned long int to_int(char *s)
+/**
+ * to_string - convert array of integers to string
+ *
+ * @arr: array of integers
+ * Return: pointer to string
+ */
+char *to_string(int arr[])
 {
-	int i = 0;
-	unsigned long int number = 0;
+	int i, size;
+	char *new_arr;
 
-	for (; s[i] != '\0'; i++)
-		number = (number * 10) + (s[i] - '0');
-	return (number);
+	while (arr)
+		i++;
+	size = i + 1;
+	new_arr = malloc(sizeof(char) * size);
+
+	for (i = 0; i <= size; i++)
+		new_arr[i] = arr[i];
+	return (new_arr);
 }
 
-void to_string(char *r, unsigned long int result)
+/**
+ * compute - converts string of numbers to int array and adds them
+ *
+ * @n1: pointer to first string of numbers
+ * @n2: pointer to second string of numbers
+ * @size_of_n1: size of first string
+ * @size_of_n2: size of second string
+ * Return: pointer to int array
+ */
+int *compute(char *n1, char *n2, int size_of_n1, int size_of_n2)
 {
-	int i, rem, len = 0;
-	unsigned long int n = result;
+	int i, j, carry = 0, sum = 0;
+	int *a, *b, *keep;
 
-	while (n != 0)
+	a = malloc(sizeof(int) * size_of_n1);
+	b = malloc(sizeof(int) * size_of_n2);
+	keep = malloc(sizeof(int) * size_of_n1);
+
+	for (i = 0; n1[i] != '\0'; i++)
+		a[i] = n1[i];
+	for (j = 0; n2[j] != '\0'; j++)
+		b[j] = n2[j];
+
+	for (i = size_of_n2 - 1; i >= 0; i--)
 	{
-		n /= 10;
-		++len;
+		sum = a[size_of_n1 - 1] + b[i] + carry;
+		keep[size_of_n1] = sum % 10;
+		carry = sum / 10;
+		size_of_n1--;
 	}
 
-	for (i = 0; i < len; i++)
+	for (; size_of_n1 > 0; size_of_n1--)
 	{
-		rem = result % 10;
-		result /= 10;
-		r[len - (i + 1)] = rem + '0';
+		sum = a[size_of_n1 - 1] + carry;
+		keep[size_of_n1] = sum % 10;
+		carry = sum / 10;
 	}
-	r[len] = '\0';
+
+	keep[size_of_n1] = carry;
+	return (keep);
 }
 
+/**
+ * check_size - compares the length of two string of numbers
+ *
+ * @n1: pointer to first string of numbers
+ * @n2: pointer to second string of numbers
+ * @size_of_n1: size of first string
+ * @size_of_n2: size of second string
+ * Return: pointer to int array
+ */
+int *check_size(char *n1, char *n2, int size_of_n1, int size_of_n2)
+{
+	if (size_of_n1 >= size_of_n2)
+		return (compute(n1, n2, size_of_n1, size_of_n2));
+	else
+		return (compute(n2, n1, size_of_n2, size_of_n1));
+}
+
+/**
+ * infinite_add - adds two numbers
+ *
+ * @n1: pointer to first string of numbers
+ * @n2: pointer to second string of numbers
+ * @r: buffer to store result
+ * @size_r: size of buffer
+ * Return: pointer to string
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
-	unsigned long int num1 = to_int(n1);
-	unsigned long int num2 = to_int(n2);
-	unsigned long int result = num1 + num2;
+	int i, j, size_of_n1, size_of_n2;
 
-	to_string(r, result);
-
-	for (; r[i] != '\0'; ++i)
+	/* to get size of n1 */
+	for (i = 0; n1[i] != '\0'; i++)
 		;
-	if (i + 1 > size_r)
+	size_of_n1 = i + 1;
+
+	/* to get size of n2 */
+	for (j = 0; n2[j] != '\0'; j++)
+		;
+	size_of_n2 = j + 1;
+
+	r = to_string(check_size(n1, n2, size_of_n1, size_of_n2));
+	for (i = 0; r[i] != '\0'; i++)
+		;
+	if (i + 1 >= size_r)
 		return (0);
-	else
-		return (r);
+
+	return (r);
 }
