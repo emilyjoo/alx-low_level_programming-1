a03a00478ef660a45a8abc1ed18981492306a107
Brightini <brightiniabasi@gmail.com>
2023-01-05 02:18:28 -0800

    Refactor code :hammer:

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
 
-	for (; s[i] != '\0'; i++)
-		_putchar(s[i]);
-	_putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+		_putchar('\n');
 }
