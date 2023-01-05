d49372135324bcdfc465484b4b1cd560b67f5c81
Brightini <brightiniabasi@gmail.com>
2023-01-05 08:35:36 -0800

    Remove loop :fire: Refactor code :hammer:

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,24 @@
 #include "main.h"
+#include <string.h>
+
+/**
+* check - uses recursion to check for palindrome
+*
+* @s: pointer to char (string to be checked)
+* @length: length of string
+* Return: 1 if s is a palindrome, otherwise 0
+*/
+int check(char *s, int length)
+{
+	if (length == 0 || length == 1)
+		return (1);
+	if (s[0] == s[length - 1])
+	{
+		length--;
+		return (check(s + 1, length - 1));
+	}
+	return (0);
+}
 
 /**
  * is_palindrome - checks if s is a palindrome
@@ -8,27 +28,10 @@
  */
 int is_palindrome(char *s)
 {
-	int i, size, check;
+	int length = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	size = i;
-	if (size == 0)
-		return (1);
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == s[size - 1])
-		{
-			check = 1;
-			size--;
-		}
-		else
-		{
-			check = 0;
-			break;
-		}
-	}
-	if (check == 1)
-		return (1);
-	return (0);
+	length = strlen(s);
+	if (check(s, length) == 0)
+		return (0);
+	return (1);
 }
