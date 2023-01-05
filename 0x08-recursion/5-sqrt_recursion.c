0354e9a3f2f0e021bbeb3c80706cb641e41d53fc
Brightini <brightiniabasi@gmail.com>
2023-01-05 07:32:39 -0800

    Refactor code :hammer: Remove loop :fire:

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,54 @@
 #include "main.h"
 
 /**
+ * compute_prime - checks for prime number recursively
+ *
+ * @n: number to be checked
+ * @i: variable used to check
+ * Return: 1 if n is a prime number, otherwise 0
+ */
+int compute_prime(int n, int i)
+{
+	if (i >= n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (compute_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - checks if n is a prime number
+ *
+ * @n: number to be checked
+ * Return: 1 if n is a prime number, otherwise 0
+ */
+int is_prime_number(int n)
+{
+
+	if (n <= 0 || n == 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (compute_prime(n, 2));
+}
+
+/**
+ * func - computes the natural square root of a number recursively
+ *
+ * @n: (int) number whose square root is to be computed
+ * @guess: (int) a number greater than 0 used to compute
+ * Return: the natural square root n
+ */
+
+int func(int n, int guess)
+{
+	if (guess * guess == n && (guess + 1) * (guess + 1) > n)
+		return (guess);
+	else
+		return (func(n, (guess + n / guess) / 2));
+}
+
+/**
  * _sqrt_recursion - computes the natural square root of a number
  *
  * @n: (int) number whose natural square root is to be computed
@@ -8,20 +56,12 @@
  */
 int _sqrt_recursion(int n)
 {
-	int sqrt, temp;
+	int i = 0;
 
-	sqrt = n / 2;
-	temp = 0;
 	if (n == 1)
 		return (1);
-	if (n < 0)
+	if (n < 0 || is_prime_number(n) == 1)
 		return (-1);
-	while (sqrt != temp)
-	{
-		temp = sqrt;
-		sqrt = (n / temp + temp) / 2;
-		if (sqrt * sqrt == n)
-			return (sqrt);
-	}
-	return (-1);
+	i = func(n, 1);
+	return (i);
 }
