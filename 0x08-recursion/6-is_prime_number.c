0ec48201bb183e7a1a2d06139d01502cc6311ac0
Brightini <brightiniabasi@gmail.com>
2023-01-05 06:36:25 -0800

    Refactor code :hammer:

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * compute_prime - checks for prime number recursively
+ *
+ * @n: number to be checked
+ * @i: variable used to check
+ * Return: 1 if n is a prime number, otherwise 0
+ */
+int compute_prime(int n, int i)
+{
+	if (i >= n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (compute_prime(n, i + 1));
+}
+
+/**
  * is_prime_number - checks if n is a prime number
  *
  * @n: number to be checked
@@ -12,10 +28,10 @@ int is_prime_number(int n)
 
 	if (n <= 0 || n == 1)
 		return (0);
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+	if (n == 2)
+		return (1);
+	i = compute_prime(n, 2);
+	if (i == 0)
+		return (0);
 	return (1);
 }
