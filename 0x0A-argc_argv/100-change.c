ab1b2245877a3e53132204fb3baf77828cf9b369
Brightini <brightiniabasi@gmail.com>
2023-01-08 23:57:18 +0100

    Add advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,37 @@
 #include "main.h"
 
 /**
- * main - prints the minimum number of coins to make
+ * main - prints the minimum cent of coins to make
  * change for an amount of money
  *
- * @argc: number of argument
+ * @argc: cent of argument
  * @argv: pointer to array of arguments
  * Return: 0 on success, otherwise 1
 */
 int main(int argc, char **argv)
 {
-	int cents;
-	int coins = 0;
-
+	int cent, coin;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(EXIT_FAILURE);
 	}
-
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
+	cent = atoi(argv[1]);
+	if (cent == 0 || cent < 0)
 		printf("0\n");
-
-	coins += cents / 25;
-	cents %= 25;
-
-	coins += cents / 10;
-	cents %= 10;
-
-	coins += cents / 5;
-	cents %= 5;
-
-	coins += cents / 2;
-	cents %= 2;
-
-	coins += cents;
-
-	printf("%d\n", coins);
-	exit(EXIT_SUCCESS);
+	while (cent > 0)
+	{
+		if (cent >= 25)
+			cent -= 25;
+		if (cent >= 10)
+			cent -= 10;
+		if (cent >= 5)
+			cent -= 5;
+		if (cent >= 2)
+			cent -= 2;
+		if (cent >= 1)
+			cent -= 1;
+		coin++; /* incremented every time a change is made */
+	}
+	printf("%d\n", coin);
 }
