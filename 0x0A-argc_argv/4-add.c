55daa3d648d8a5b33ee33d0e4811c332c564766c
Brightini <brightiniabasi@gmail.com>
2023-01-09 01:18:03 +0100

    Fix non-digit bugs :bugs:

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,23 +6,25 @@
  * @argc: number of arguments
  * @argv: pointer to array of arguments
  * Return: 0 on success, otherwise 1
-*/
+ */
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int row, col, sum = 0;
 
 	if (argc > 0)
 	{
-		for (i = argc - 1; i > 0; i--)
+		/* checks if argument is a number*/
+		for (row = argc - 1; row > 0; row--)
 		{
-			/* checks if argument is a number*/
-			if (atoi(argv[i]) && atoi(argv[i]) > 0)
-				sum += atoi(argv[i]);
-			else
+			for (col = 0; argv[row][col] != '\0'; col++)
 			{
-				printf("Error\n");
-				exit(EXIT_FAILURE);
+				if (!(isdigit(argv[row][col])) || atoi(argv[row]) < 0)
+				{
+					printf("Error\n");
+					exit(EXIT_FAILURE);
+				}
 			}
+			sum += atoi(argv[row]);
 		}
 		printf("%d\n", sum);
 		exit(EXIT_SUCCESS);
