578695563dca4a131da3721c8ee5cf05d5e132db
Brightini <brightiniabasi@gmail.com>
2023-01-10 19:19:27 +0100

    Fixed memory size bug :bug:

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 	/* allocate memory for the whole array */
-	grid = malloc(sizeof(int) * height);
+	grid = malloc(sizeof(int) * height * width);
 	if (!grid)
 		return (NULL);
 
