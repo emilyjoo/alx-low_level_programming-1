d3c4715427a172f952e13d8a4f70823b80242d88
Brightini <brightiniabasi@gmail.com>
2023-01-12 20:21:13 +0100

    Update array size :ok_hand:

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	array_size = min + max + 1;
+	array_size = (max - min) + 1;
 	ptr = malloc(sizeof(int) * array_size);
 	if (!ptr)
 		return (NULL);
