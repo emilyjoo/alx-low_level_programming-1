39a5ec89acca863f5f7499acd711d871434b1210
Brightini <brightiniabasi@gmail.com>
2023-01-16 17:25:42 +0100

    Update condition for float value of 0 :ok_hand:'

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (!d->age)
+		if (!d->age && d->age != 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
