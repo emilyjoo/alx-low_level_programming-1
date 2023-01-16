483c421032037597a555191d55d07fa0444c1e87
Brightini <brightiniabasi@gmail.com>
2023-01-16 17:19:06 +0100

    Add conditions for function parameters

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,13 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		if (name)
+			d->name = name;
+		if (age)
+			d->age = age;
+		if (owner)
+			d->owner = owner;
+	}
 }
