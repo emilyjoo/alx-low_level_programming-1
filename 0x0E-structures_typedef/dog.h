c3bb1c8e4f52a55fe112e20c8d31536052cf46e8
Brightini <brightiniabasi@gmail.com>
2023-01-16 18:21:41 +0100

    Redefine struct dog type :zap:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - user-defined variable for dog objects
  *
@@ -8,17 +10,18 @@
  * @age: age of dog object
  * @owner: owner of dog object
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 /* function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 
 #endif
