cb9cc7d4647077b87c9535d83905d8c44b658b37
Brightini <brightiniabasi@gmail.com>
2023-01-16 18:22:12 +0100

    Add new task :tada:

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,69 @@
+#include "dog.h"
+
+/**
+ * _strcpy - copies a string
+ *
+ * @dest: destination string
+ * @src: source string
+ */
+void _strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+}
+
+
+/**
+ * _strlen - computes the length of a string
+ *
+ * @s: pointer to string
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * new_dog - create a new dog variable
+ *
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ *
+ * Return: NULL if function fails, otherwise, a pointer to the new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+	char *name_copy, *owner_copy;
+
+	/* + 1 for null terminator */
+	name_copy = malloc(_strlen(name) + 1);
+	if (!name_copy)
+		return (NULL);
+	owner_copy = malloc(_strlen(owner) + 1);
+	if (!owner_copy)
+		return (NULL);
+
+	_strcpy(name_copy, name);
+	_strcpy(owner_copy, owner);
+
+	newDog = malloc(sizeof(dog_t));
+	if (!newDog)
+		return (NULL);
+
+	/* initialise values for new dog */
+	newDog->name = name_copy;
+	newDog->age = age;
+	newDog->owner = owner_copy;
+
+	return (newDog);
+}
