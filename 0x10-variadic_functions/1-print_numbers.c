3ac4a510005382b388188605f4699b357e0dba16
Brightini <brightiniabasi@gmail.com>
2023-01-19 18:39:35 +0100

    Revert changes :rewind:

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	size_t i = 0;
-	int num;
 
 	if (n <= 0 || !n)
 		return;
@@ -19,10 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (; i < n; i++)
 	{
-		num = va_arg(args, int);
-		if (num)
-			printf("%d", num);
-		if (separator && i < n - 1 && num)
+		printf("%d", va_arg(args, int));
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(args);
