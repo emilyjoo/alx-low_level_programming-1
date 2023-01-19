8d4ee357bfab9a885ecf9f955bfc2a73126a2824
Brightini <brightiniabasi@gmail.com>
2023-01-19 18:31:21 +0100

    Add condition for edge case :ok_hand:

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	size_t i = 0;
+	int num;
 
 	if (n <= 0 || !n)
 		return;
@@ -18,7 +19,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		num = va_arg(args, int);
+		if (num)
+			printf("%d", num);
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
