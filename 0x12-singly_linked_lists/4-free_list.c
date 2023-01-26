4b5967313e6005803cdef3ddc8b02546dd39871c
Brightini <brightiniabasi@gmail.com>
2023-01-26 20:30:52 +0100

    Fixed bug :bug:

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,16 +7,21 @@
 */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *old, *new;
 
+	/* nothing to free */
 	if (!head)
 		return;
-	temp = head;
-	/* free malloc'ed string */
-	while (temp->next)
+
+	/* set both old and new to head to keep track of next node */
+	old = head;
+	new = head;
+
+	while (new->next)
 	{
-		free(temp->str);
-		free(temp);
-		temp = temp->next;
+		new = new->next; /* now points to the next node */
+		free(old->str); /* frees malloc'ed of previous node */
+		free(old); /* frees the of previous node */
+		old = new; /* then assign new node to old to be free */
 	}
 }
