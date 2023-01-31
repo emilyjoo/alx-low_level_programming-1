88ca65269b705b1f4530d2aa887ee69352558251
Brightini <brightiniabasi@gmail.com>
2023-01-31 11:53:43 +0100

    Minor changes :ok_hand:

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - prints all the elements in a list
@@ -12,7 +13,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *temp;
 
 	temp = h;
-	while (temp)
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		count++;
