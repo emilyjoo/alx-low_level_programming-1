0d27a3ef76f95820d22429284d15111a491fb815
Brightini <brightiniabasi@gmail.com>
2023-01-31 12:55:55 +0100

    Working on edge case(s) :construction:

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,20 +5,31 @@
  *
  * @head: pointer to address of head pointer
  * @index: index position
- * Return: 0 if list is empty, otherwise node at position @index
-*/
+ * Return: NULL if list is empty or node doesn't exist,
+ * otherwise node at position @index
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp;
-	unsigned int i;
+	listint_t *temp, *ptr;
+	unsigned int i, count = 0;
 
 	if (!head)
 		return (NULL);
 
 	temp = head;
-	for (i = 0; i < index; i++)
-		temp = temp->next;
-
-	/* at this point, temp points to the node at index position */
-	return (temp);
+	ptr = head;
+	/* to get length of list */
+	while (ptr->next)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	if (index <= count) /* when node exists */
+	{
+		for (i = 0; i < index; i++)
+			temp = temp->next;
+		/* at this point, temp points to the node at index position */
+		return (temp);
+	}
+	return (NULL);
 }
