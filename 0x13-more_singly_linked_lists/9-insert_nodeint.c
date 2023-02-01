dcc2aebc7580415ec7a93fc64ae05a1143b2ed2b
Brightini <brightiniabasi@gmail.com>
2023-02-01 14:52:01 +0100

    Refactor code :hammer:

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,30 +1,6 @@
 #include "lists.h"
 
 /**
- * listint_len - computes the number of elements in a list
- *
- * @h: head pointer to list
- * Return: 0 if list is empty, otherwise number of elements in a list
- */
-size_t listint_len(const listint_t *h)
-{
-	size_t count = 0;
-	const listint_t *temp;
-
-	/*for an empty list */
-	if (!h)
-		return (0);
-
-	temp = h;
-	while (temp)
-	{
-		count++;
-		temp = temp->next;
-	}
-	return (count);
-}
-
-/**
  * insert_nodeint_at_index - inserts a new node at an index position
  *
  * @head: pointer to address of head pointer
@@ -34,23 +10,16 @@ size_t listint_len(const listint_t *h)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	size_t list_len, i;
+	unsigned int list_len = 0;
 	listint_t *temp, *new_node;
-
-	/* get length of list */
-	list_len = listint_len(*head);
-
-	/* 'list_len == 0' for empty list */
-	if (idx > list_len || list_len == 0)
-		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = NULL;
 
-	temp = *head;
-
+	/* inserts at the beginning */
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -58,12 +27,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		for (i = 1; i < idx; i++)
+		temp = *head;
+		while (list_len < idx - 1)
+		{
+			list_len++;
 			temp = temp->next;
-
+			/* when temp has gone out of the list.*/
+			/* e.g. in the case of idx being negative */
+			if (!temp)
+				return (NULL);
+		}
 		/* temp is now at idx position */
 		new_node->next = temp->next;
 		temp->next = new_node;
+		return (new_node);
 	}
 	return (new_node);
 }
