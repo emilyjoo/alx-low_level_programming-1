6f74b48ed06b422824b6e0ecfff0b4849688bcf7
Brightini <brightiniabasi@gmail.com>
2023-02-08 21:58:07 +0100

    Minor changes :ok_hands:

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @filename: name of file to be created
  * @text_content: content to be writen to @filename
  * Return: On success 1, otherwise -1
-*/
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename,  O_CREAT | O_TRUNC | O_RDWR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
@@ -24,12 +24,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (; text_content[i] != '\0'; i++) /* to get length of string */
 			;
+		/* write to file */
+		num_bytes = write(fd, text_content, i);
+		if (num_bytes == -1)
+			return (-1);
 	}
-
-	/* write ti file */
-	num_bytes = write(fd, text_content, i);
-	if (num_bytes == -1)
-		return (-1);
 
 	close(fd);
 	return (1);
