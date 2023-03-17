e7d215314497beb1e0225e2c96d12516440e7223
Brightini <brightiniabasi@gmail.com>
2023-03-17 18:52:07 +0100

    Fix: condition for empty list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 
 	if (!*head)
+	{
+		new_node->next = *head;
 		*head = new_node;
+	}
 	else
 	{
 		new_node->next = *head;
