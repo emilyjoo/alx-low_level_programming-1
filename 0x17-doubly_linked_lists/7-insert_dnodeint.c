379ba58b10776f6deaffdf5d210479032e2499c4
Brightini <brightiniabasi@gmail.com>
2023-03-19 20:44:21 +0100

    Fix: condition for empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 
 	if (!*h) /* for empty list */
-		return (NULL);
+	{
+		*h = new_node;
+		return (new_node);
+	}
 
 	if (idx == 0) /* to insert at the beginning of the list */
 	{
